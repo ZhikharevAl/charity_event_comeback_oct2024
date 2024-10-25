cdc26b96f1c27568adebaf706610c67f45ef4b81
Evgeny Sokolov-Viluzin <jesovile@gmail.com>
2024-10-26 00:18:03 +0400

    OpenAPI spec update

--- index.ts
+++ index.ts
@@ -27,6 +27,10 @@ const requestRepository = new HelpRequestRepository();
 const userRepository = new UserRepository(requestRepository);
 const authRepository = new AuthRepository(userRepository.getUsers());
 
+// OpenAPI specification
+const openApiDocumentation = YAML.load('./apispec2.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
+
 // ERROR MIDDLEWARE
 let ERROR_REQUEST_NUMBER = 1;
 const errorMiddleware = (req: Request<never>, res: Response, next: NextFunction) => {
@@ -54,12 +58,6 @@ const authMiddleware = (req: Request<never>, res: Response, next: NextFunction) 
     req.userId = userId;
     next();
 };
-
-
-// OpenAPI specification
-const openApiDocumentation = YAML.load('./apispec.yaml');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
-
 
 app.post('/api/auth', (req: Request, res: Response) => {
     const { login, password } = req.body;
